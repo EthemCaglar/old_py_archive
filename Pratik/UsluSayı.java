867f5a7ef7701916afb2dbf4ae28ecb97296b9e4
EthemCaglar <142922479+EthemCaglar@users.noreply.github.com>
2023-09-12 19:47:39 +0300

    Add files via upload

---
+++ Pratik/UsluSayı.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class UsluSayı {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // verilerin toplanması
+        System.out.print("Verdiğiniz Sayının Verdiğiniz Değerdeki Üssü\n");
+        System.out.print("Taban Sayıyı Giriniz: ");
+        int taban = input.nextInt();
+        System.out.print("Kuvveti Giriniz: ");
+        int ust = input.nextInt();
+        int sonuc = 1;
+
+        for(int i=ust; 0 < i ;i--){
+            sonuc *= taban;
+        }
+        System.out.print(taban + " üstü " + ust + ": " + sonuc);
+    }
+}
