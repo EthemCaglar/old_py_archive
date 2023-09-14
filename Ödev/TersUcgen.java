6a6f64612148b95e10158a14090d8f14cf2ba90a
EthemCaglar <142922479+EthemCaglar@users.noreply.github.com>
2023-09-14 11:28:18 +0300

    Update TersUcgen.java

--- Ödev/TersUcgen.java
+++ Ödev/TersUcgen.java
@@ -5,17 +5,15 @@ public class TersUcgen {
         System.out.print("Kaç Yıldızlı Olsun: ");
         int basamak = input.nextInt();
 
-        int tur = 0;
         int yıldız= basamak*2-1;
 
-        while(tur != basamak){ // merkezi de dahil etmek için 1 ekliyoruz
+        for(int tur = 0 ; tur != basamak ; tur++){ // merkezi de dahil etmek için 1 ekliyoruz
             for (int i = tur ; 1 <= i ; i--) {
                 System.out.print(" ");
             }
             for (int i= yıldız-tur*2 ; 1 <= i ; i--) {
                 System.out.print("*");
             }
-            tur += 1;
             System.out.print("\n");
         }
         input.close();
