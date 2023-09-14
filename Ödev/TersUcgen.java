dc5e4ea01a5c0738be1176183fee91a4dc695cf9
EthemCaglar <142922479+EthemCaglar@users.noreply.github.com>
2023-09-14 11:26:13 +0300

    Update TersUcgen.java

--- Ödev/TersUcgen.java
+++ Ödev/TersUcgen.java
@@ -3,20 +3,16 @@ public class TersUcgen {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); // verilerin toplanması
         System.out.print("Kaç Yıldızlı Olsun: ");
-        int merkez = input.nextInt();
+        int basamak = input.nextInt();
 
-        if (merkez % 2 != 1){ // Çift olursa saçma görünüyor
-            merkez += 1;
-        }
-
-        int makstur = (merkez - 1)/2; // ilk bırakılacak ve en büyük bosluk miktar
         int tur = 0;
+        int yıldız= basamak*2-1;
 
-        while(tur != makstur+1){ // merkezi de dahil etmek için 1 ekliyoruz
+        while(tur != basamak){ // merkezi de dahil etmek için 1 ekliyoruz
             for (int i = tur ; 1 <= i ; i--) {
                 System.out.print(" ");
             }
-            for (int i= merkez-tur*2 ; 1 <= i ; i--) {
+            for (int i= yıldız-tur*2 ; 1 <= i ; i--) {
                 System.out.print("*");
             }
             tur += 1;
