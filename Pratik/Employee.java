462dbc7fd17b7e32e2adb7983413c9514a6085ba
Ethem ÇAĞLAR <142922479+EthemCaglar@users.noreply.github.com>
2023-09-21 02:47:17 +0300

    Add files via upload

---
+++ Pratik/Employee.java
@@ -0,0 +1,47 @@
+package Fabrika;
+
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+
+    public Employee(String name, int salary, int workHours,int hireYear){
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public void tax(){
+        if(this.salary >= 1000){
+            this.salary = this.salary * 0.97;
+        }
+    }
+
+    public void bonus(){
+        if(this.workHours >= 40){
+            this.salary = this.salary + (this.workHours-40)*30;
+        }
+    }
+
+    public void raiseSalary(){ // şuanki yıl 2021
+        if(2021 - this.hireYear > 10){
+            this.salary = this.salary*1.1;
+        }else if(2021 - this.hireYear > 20){
+            this.salary = this.salary*1.15;
+        }else{
+            this.salary = this.salary*1.05;
+        }
+    }
+
+    public void print(){
+        System.out.println("-------Employee Profile-------");
+        System.out.println("Name: " + this.name);
+        System.out.println("Salary: " + this.salary);
+        System.out.println("Work Hours: " + this.workHours);
+        System.out.println("Hire Year: " + this.hireYear);
+    }
+}
+
+
