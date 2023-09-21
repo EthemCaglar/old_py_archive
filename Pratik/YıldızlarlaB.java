d30ef46766b5bec9d55347da9e93c5b1b83efb0a
Ethem ÇAĞLAR <142922479+EthemCaglar@users.noreply.github.com>
2023-09-21 17:38:36 +0300

    Add files via upload

---
+++ Pratik/YıldızlarlaB.java
@@ -0,0 +1,30 @@
+public class YıldızlarlaB {
+    public static void main(String[] args) {
+        String[][] letter = new String[6][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 2 || i == 5) {
+                    if(j < 3){
+                        letter[i][j] = " * ";
+                    }else{
+                        letter[i][j] = "   ";
+                    }   
+                } else if (i == 1 || i == 3 || i == 4) {
+                    if(j == 0 || j == 3){
+                        letter[i][j] = " * ";
+                    }else{
+                        letter[i][j] = "   ";
+                    }
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
