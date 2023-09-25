b0caa0b4113842c67c116206384186b91fff62c5
Ethem ÇAĞLAR <142922479+EthemCaglar@users.noreply.github.com>
2023-09-25 20:03:01 +0300

    Update MatrisTranspoz.java

--- Ödev/MatrisTranspoz.java
+++ Ödev/MatrisTranspoz.java
@@ -10,5 +10,19 @@ public class MatrisTranspoz {
                 transpoz[j][i] = matris[i][j]; // satırla sutünun değiştirmek için i-j yerine j-i
             }
         }
+        System.out.print("Matris:\n");
+        for(int i=0 ; i < matris.length ; i++){ // satırdaki elemansayısı
+            for(int j=0 ; j < matris[0].length ; j++){
+                System.out.print(matris[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\nTranspoz:\n");
+        for(int i=0 ; i < transpoz.length ; i++){ // satırdaki elemansayısı
+            for(int j=0 ; j < transpoz[0].length ; j++){
+                System.out.print(transpoz[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
